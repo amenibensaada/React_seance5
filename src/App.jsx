32e0ad5ed2ad66975cb61af1439570ba35d81509
amenibensaada <amenisaada19@gmail.com>
2024-02-21 23:18:22 -1100

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 import useWindowWidth from './CustomHooks/useWindowWidth'
 
 import "./App.css";
-import ClassComponent from "./Composants/ClassComponent";
-import FunctionalComponent from "./Composants/FunctionalComponent";
+// import ClassComponent from "./Composants/ClassComponent";
+// import FunctionalComponent from "./Composants/FunctionalComponent";
 import Mouting from "./lifeCycle[CC]/Mounting";
 import { Update } from "./lifeCycle[CC]/Update";
 import Unmouting from "./lifeCycle[CC]/Unmounting";
 import { Route, Routes } from 'react-router-dom';
-import FcLifeCycle from './lifeCycle[FC]/FcLifeCycle';
-
+// import FcLifeCycle from './lifeCycle[FC]/FcLifeCycle';
+// import NotFound from './Composants/NotFound';
+const FcLifeCycle = React.lazy(()=> import("./lifeCycle[FC]/FcLifeCycle"))
+const FunctionalComponent = React.lazy(()=> import("./Composants/FunctionalComponent"))
+const NotFound = React.lazy(()=> import("./Composants/NotFound"))
+const ClassComponent = React.lazy(()=> import("./Composants/ClassComponent"))
 
 function App() {
   const [{color , background},setColor] =
@@ -29,14 +33,17 @@ function App() {
   <p> the color is : {color} {background}</p> 
 
     {res.width } {res.height} */}
+      <Suspense fallback={<p> Waiting server </p>}>
       <Routes>
         <Route path="/routes">
           <Route index element={<FunctionalComponent/>}/>
           
         <Route path="updating/:id" element={<FcLifeCycle />} />
           <Route path="classComponent" element={<ClassComponent />} />
-          </Route>
+        </Route>
+        <Route path="*" element={ <NotFound/>} />
         </Routes>
+        </Suspense>
     </>
   );
 }
